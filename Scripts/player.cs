97d832522526a87f3d6496aa229022abb16757ff
Armandulis <32477822+Armandulis@users.noreply.github.com>
2023-11-02 23:14:12 +0100

    added animation and movement

--- Scripts/player.cs
+++ Scripts/player.cs
@@ -1,31 +1,89 @@
 using Godot;
 using System;
+using System.Security.AccessControl;
+using System.Threading;
 using System.Xml.Schema;
 
 public partial class player : CharacterBody2D
 {
-	public override void _Process(double delta)
+	public AnimatedSprite2D animatedSprite2D;
+
+	private string currentDirrection = "down";
+    private string currentAnimation = "";
+	private bool isRunning = false;
+
+    public override void _Ready()
+    {
+		this.animatedSprite2D = (AnimatedSprite2D) FindChild("AnimatedSprite2D");
+		this.HandleAnimations();
+    }
+    public override void _Process(double delta)
 	{
-		if(Input.IsKeyPressed( Key.W))
+	}
+
+	public override void _PhysicsProcess(double delta)
+	{
+		this.isRunning = false;
+
+		if (Input.IsKeyPressed(Key.W))
 		{
-			this.Position += new Vector2(0,-5);
+			this.currentDirrection = "up";
+			this.Position += new Vector2(0, -5);
+			this.isRunning = true;
+		}
+		if (Input.IsKeyPressed(Key.S))
+		{
+			this.currentDirrection = "down";
+			this.Position += new Vector2(0, 5);
+			this.isRunning = true;
+		}
+		if (Input.IsKeyPressed(Key.A))
+		{
+			this.currentDirrection = "left";
+			this.Position += new Vector2(-5, 0);
+			this.isRunning = true;
+		}
+		if (Input.IsKeyPressed(Key.D))
+		{	
+			this.currentDirrection = "right";
+			this.Position += new Vector2(5, 0);
+			this.isRunning = true;
 		}
 		
-		if(Input.IsKeyPressed( Key.S))
+		this.HandleAnimations();
+	}
+
+	public void HandleAnimations()
+	{
+		if(this.isRunning == true && this.currentAnimation != "running")
 		{
-			this.Position += new Vector2(0,5);
+			this.currentAnimation = "running";
+			this.animatedSprite2D.Play(this.currentAnimation);
+		}
+		if( this.isRunning == false && this.currentAnimation != "idle" )
+		{
+			this.currentAnimation = "idle";
+			this.animatedSprite2D.Play(this.currentAnimation);
 		}
 
-		
-		if(Input.IsKeyPressed( Key.A))
+
+		if (this.currentDirrection == "up")
 		{
-			this.Position += new Vector2(-5,0);
 		}
 
-		
-		if(Input.IsKeyPressed( Key.D))
+		if (this.currentDirrection == "down")
 		{
-			this.Position += new Vector2(5,0);
+		}
+
+		if (this.currentDirrection == "right")
+		{
+			this.animatedSprite2D.FlipH = false;
+		}
+
+
+		if (this.currentDirrection == "left")
+		{
+			this.animatedSprite2D.FlipH = true;
 		}
 	}
 }
