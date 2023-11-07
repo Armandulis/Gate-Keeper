040e4cfb5f8e10c2c3b3ba8aa2e97d252e1102ca
Armandulis <32477822+Armandulis@users.noreply.github.com>
2023-11-07 23:23:38 +0100

    modular actions backbone

--- Scripts/player.cs
+++ Scripts/player.cs
@@ -1,22 +1,34 @@
 using Godot;
 using System;
+using System.Reflection;
 using System.Security.AccessControl;
 using System.Threading;
 using System.Xml.Schema;
 
-public partial class player : CharacterBody2D
+public partial class Player : CharacterBody2D
 {
 	public AnimatedSprite2D animatedSprite2D;
 
 	private string currentDirrection = "down";
     private string currentAnimation = "";
 	private bool isRunning = false;
-	private float speed = 200;
+
+	private bool isInAttachRangeEnemy = false;
+	private bool isInCooldownEnemy = false;
+	private float health = 100;
+	private bool isAlive = true;
+	private Godot.Timer attackCooldown;
+	private Godot.Timer playerAttackCooldown;
+	private Move move;
 
     public override void _Ready()
     {
+		this.attackCooldown = (Godot.Timer) FindChild("AttackCooldown");
 		this.animatedSprite2D = (AnimatedSprite2D) FindChild("AnimatedSprite2D");
 		this.HandleAnimations();
+		this.playerAttackCooldown = (Godot.Timer) FindChild("PlayerAttackCooldown");
+
+		this.move = LoadAction();
     }
     public override void _Process(double delta)
 	{
@@ -25,8 +37,11 @@ public partial class player : CharacterBody2D
 
 	public override void _PhysicsProcess(double delta)
 	{
-		Vector2 inputDirection = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
-        Velocity = inputDirection * speed;
+		this.move.Execute(this);
+		this.EnemyAttack();
+		this.Attack();
+		// Vector2 inputDirection = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+        // Velocity = inputDirection * speed;
         // var collision = MoveAndCollide(Velocity*(float)delta);
 		MoveAndSlide();
 		this.isRunning = false;
@@ -92,5 +107,84 @@ public partial class player : CharacterBody2D
 		{
 			this.animatedSprite2D.FlipH = true;
 		}
+	}
+
+	public void PlayerClass()
+	{
+		
+	}
+
+	public void Attack()
+	{
+		if( Input.IsKeyPressed(Key.F) )
+		{
+			Core.instance.isPlayerAttacking = true;
+			this.playerAttackCooldown.Start();
+		}
+	}
+
+	public void EnemyAttack()
+	{
+		if(this.isInAttachRangeEnemy && !isInCooldownEnemy )
+		{
+			this.health -= 10;
+			this.isInCooldownEnemy = true;
+			this.attackCooldown.Start();
+			GD.Print(this.health);
+			if( this.health <= 0 )
+			{
+				this.isAlive = false;
+			}
+		}
+	}
+	
+
+	public void OnHitboxEntered(Node2D body)
+	{
+		if(body.HasMethod("Enemy"))
+		{
+			isInAttachRangeEnemy = true;
+		}
+	}
+
+	
+	public void OnHitboxExited(Node2D body)
+	{
+		if(body.HasMethod("Enemy"))
+		{
+			isInAttachRangeEnemy = false;
+		}
+	}
+
+	public void OnAttackCooldownTimeout()
+	{
+		this.isInCooldownEnemy = false;
+	}
+
+	public void OnPlayerAttackCooldownTimeout()
+	{
+			this.playerAttackCooldown.Stop();
+			Core.instance.isPlayerAttacking = false;
+	}
+
+	public Move LoadAction()
+	{
+		PackedScene scene = GD.Load<PackedScene>("res://Actions/Move/Move.tscn");
+		Node sceneNode = scene.Instantiate();
+		AddChild(sceneNode);
+
+		return (Move)sceneNode;
+			//  if(sceneNode.GetScript().Obj.GetType().FullName is Move moveScript)
+		// {
+		// 	return moveScript;
+		// }
+
+		// return null;
+	
+	}
+
+	public Variant GetNodeScript(Node scene)
+	{
+		return scene.GetScript();
 	}
 }
