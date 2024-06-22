0658f39c699d63e09ce1febfbd74d9d3937115c3
Armandulis <32477822+Armandulis@users.noreply.github.com>
2024-06-22 13:45:07 +0200

    godot changes

---
+++ Components/Spells/SpellCastManager/SpellCastManagerComponent.cs
@@ -0,0 +1,58 @@
+using Godot;
+using System;
+using System.Reflection.Metadata.Ecma335;
+
+public partial class SpellCastManagerComponent : Node2D
+{
+	[Export]
+	public Timer globalCooldownTimer;
+
+    private bool isInGlobalCooldown = false;
+    public bool isCasting = false;
+
+	[Export]
+	public Player player;
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
+    {
+	}
+
+	public bool castInterupted()
+	{
+		return player.isMoving;
+	}
+
+	public void spellCasted()
+	{
+		isCasting = true;
+		this.isInGlobalCooldown = true;
+		globalCooldownTimer.Start( 0.5 );
+	}
+
+	public void finishedCastingSpell()
+	{
+		isCasting = false;
+	}
+
+	public bool tryCastSpell()
+	{
+		if( !isInGlobalCooldown && !isCasting )
+		{
+			spellCasted();
+			return true;
+		}
+		return false;
+	}
+
+	public void _OnGlobalCooldownTimeout()
+	{
+		this.isInGlobalCooldown = false;
+	}
+
+}
